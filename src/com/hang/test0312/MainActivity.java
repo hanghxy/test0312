73aa94b32451096e045eeeea75083215dccdb231
hanghxy <tiantanggo@163.com>
2015-03-13 13:25:15 +0800

    MainActivity修改

--- src/com/hang/test0312/MainActivity.java
+++ src/com/hang/test0312/MainActivity.java
@@ -2,11 +2,14 @@ package com.hang.test0312;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
+		
+		Toast.makeText(getApplicationContext(), "11", 0);
 	}
 }
