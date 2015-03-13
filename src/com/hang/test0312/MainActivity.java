fd9013f8108f1a0266cd5c306e1cf4a9e5b0791d
hanghxy <tiantanggo@163.com>
2015-03-13 12:12:13 +0800

    首次修改

---
+++ src/com/hang/test0312/MainActivity.java
@@ -0,0 +1,12 @@
+package com.hang.test0312;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+	}
+}
